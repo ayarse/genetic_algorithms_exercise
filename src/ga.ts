d7640fdbcf1799318e3f418453f0fab17c6f2669
Ayas Nasih <ayasnasih@gmail.com>
2022-08-31 22:50:18 +0500

    refactor: extract ga algos to separate file

---
+++ src/ga.ts
@@ -0,0 +1,106 @@
+import { plot, Plot } from "nodeplotlib";
+import { Generation } from "./classes/Generation";
+import { Rule } from "./classes/Rule";
+import { RunConfig } from "./util/config";
+import { crossover, mutation, tournamentSelection } from "./util/helpers";
+
+interface GAConfig {
+    config: RunConfig;
+    globalRules: Rule[];
+    epochs: number;
+}
+
+export const findBestFitness = ({ config, globalRules, epochs }: GAConfig) => {
+
+    const generations: Generation[] = [];
+    generations.push(new Generation(config));
+    generations[0].fitness_func(globalRules);
+    let max = 0;
+    const plotData: number[] = [];
+    const plotBestFit: number[] = [];
+
+    for (let i = 0; i < epochs; i++) {
+        let offspring = tournamentSelection(generations[0], config);
+        offspring = crossover(offspring, config);
+        offspring = mutation(offspring, config);
+        generations.push(new Generation(config, offspring));
+        if (generations[i].fittest > max) {
+            max = generations[i].fittest;
+        }
+        console.log(`Generation: ${i} Fittest: ${max}`);
+        plotData.push(i);
+        plotBestFit.push(max);
+        generations[i + 1].fitness_func(globalRules);
+    }
+
+    const data: Plot[] = [
+        {
+            x: plotData,
+            y: plotBestFit,
+            type: 'scatter',
+            labels: ['Generation', 'Fittest'],
+            name: "Fittest"
+        },
+    ];
+
+    plot(data, { title: `Best Fitness Over ${generations.length - 1} Generations` });
+}
+
+export const findAvgBestLowFitness = ({ config, globalRules, epochs }: GAConfig) => {
+
+    const generations: Generation[] = [];
+    generations.push(new Generation(config));
+    generations[0].fitness_func(globalRules);
+    let max = 0;
+    let max2 = 0;
+    let avg = 0;
+    let low = 0;
+    const plotData: number[] = [];
+    const plotBestFit: number[] = [];
+    const plotAvgFit: number[] = [];
+    const plotLowFit: number[] = [];
+    for (let i = 0; i < epochs; i++) {
+        let offspring = tournamentSelection(generations[0], config);
+        offspring = crossover(offspring, config);
+        offspring = mutation(offspring, config);
+        generations.push(new Generation(config, offspring));
+        avg = generations[i].averageFitness;
+        max = generations[i].fittest;
+        low = generations[i].lowestFitness;
+        console.log(`Generation: ${i} Fittest: ${max} Average: ${avg} Lowest: ${low}`);
+        plotData.push(i);
+        plotBestFit.push(max);
+        plotAvgFit.push(avg);
+        plotLowFit.push(low);
+        if (generations[i].fittest > max2) {
+            max2 = generations[i].fittest;
+        }
+        generations[i + 1].fitness_func(globalRules);
+    }
+    console.log("Max Fitness: " + max2);
+    const data: Plot[] = [
+        {
+            x: plotData,
+            y: plotBestFit,
+            type: 'scatter',
+            labels: ['Generation', 'Fittest'],
+            name: "Fittest"
+        },
+        {
+            x: plotData,
+            y: plotAvgFit,
+            type: 'scatter',
+            labels: ['Generation', 'Average'],
+            name: "Average"
+        },
+        {
+            x: plotData,
+            y: plotLowFit,
+            type: 'scatter',
+            labels: ['Generation', 'Lowest'],
+            name: "Lowest"
+        },
+    ];
+
+    plot(data, { title: `Average, Best, & Lowest Fitness Over ${generations.length - 1} Generations` });
+}
--- src/index.ts
+++ src/index.ts
@@ -6,112 +6,11 @@
 import readline from 'readline';
 import path from 'path';
 import { Rule } from './classes/Rule';
-import { Generation } from './classes/Generation';
-import { crossover, mutation, readFile, tournamentSelection } from './util/helpers';
-import { plot, Plot } from 'nodeplotlib';
-import { data1Config, data2Config, data3Config, RunConfig } from './util/config';
+import { data1Config, data2Config, data3Config } from './util/config';
 import { loadData, loadDataSet3 } from './util/loaders';
+import { findAvgBestLowFitness, findBestFitness } from './ga';
 
 let globalRules: Rule[] = [];
-
-const findBestFitness = (config: RunConfig) => {
-
-    const epochs = 500;
-
-    const generations: Generation[] = [];
-    generations.push(new Generation(config));
-    generations[0].fitness_func(globalRules);
-    let max = 0;
-    const plotData: number[] = [];
-    const plotBestFit: number[] = [];
-
-    for (let i = 0; i < epochs; i++) {
-        let offspring = tournamentSelection(generations[0], config);
-        offspring = crossover(offspring, config);
-        offspring = mutation(offspring, config);
-        generations.push(new Generation(config, offspring));
-        if (generations[i].fittest > max) {
-            max = generations[i].fittest;
-        }
-        console.log(`Generation: ${i} Fittest: ${max}`);
-        plotData.push(i);
-        plotBestFit.push(max);
-        generations[i + 1].fitness_func(globalRules);
-    }
-
-    const data: Plot[] = [
-        {
-            x: plotData,
-            y: plotBestFit,
-            type: 'scatter',
-            labels: ['Generation', 'Fittest'],
-            name: "Fittest"
-        },
-    ];
-
-    plot(data, { title: `Best Fitness Over ${generations.length - 1} Generations` });
-}
-
-const findAvgBestLowFitness = (config: RunConfig) => {
-
-    const epochs = 50;
-
-    const generations: Generation[] = [];
-    generations.push(new Generation(config));
-    generations[0].fitness_func(globalRules);
-    let max = 0;
-    let max2 = 0;
-    let avg = 0;
-    let low = 0;
-    const plotData: number[] = [];
-    const plotBestFit: number[] = [];
-    const plotAvgFit: number[] = [];
-    const plotLowFit: number[] = [];
-    for (let i = 0; i < epochs; i++) {
-        let offspring = tournamentSelection(generations[0], config);
-        offspring = crossover(offspring, config);
-        offspring = mutation(offspring, config);
-        generations.push(new Generation(config, offspring));
-        avg = generations[i].averageFitness;
-        max = generations[i].fittest;
-        low = generations[i].lowestFitness;
-        console.log(`Generation: ${i} Fittest: ${max} Average: ${avg} Lowest: ${low}`);
-        plotData.push(i);
-        plotBestFit.push(max);
-        plotAvgFit.push(avg);
-        plotLowFit.push(low);
-        if (generations[i].fittest > max2) {
-            max2 = generations[i].fittest;
-        }
-        generations[i + 1].fitness_func(globalRules);
-    }
-    console.log("Max Fitness: " + max2);
-    const data: Plot[] = [
-        {
-            x: plotData,
-            y: plotBestFit,
-            type: 'scatter',
-            labels: ['Generation', 'Fittest'],
-            name: "Fittest"
-        },
-        {
-            x: plotData,
-            y: plotAvgFit,
-            type: 'scatter',
-            labels: ['Generation', 'Average'],
-            name: "Average"
-        },
-        {
-            x: plotData,
-            y: plotLowFit,
-            type: 'scatter',
-            labels: ['Generation', 'Lowest'],
-            name: "Lowest"
-        },
-    ];
-
-    plot(data, { title: `Average, Best, & Lowest Fitness Over ${generations.length - 1} Generations` });
-}
 
 const io = readline.createInterface({
     input: process.stdin,
@@ -156,7 +55,16 @@ const dataFile = (filename: string): string => {
                 break;
         }
 
-        findBestFitness(config);
-        findAvgBestLowFitness(config);
+        findBestFitness({
+            config,
+            globalRules,
+            epochs: 500,
+        });
+
+        findAvgBestLowFitness({
+            config,
+            globalRules,
+            epochs: 50
+        });
     }
 })();
