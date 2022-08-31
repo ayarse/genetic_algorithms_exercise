b30c5001d09a452842c87a903bd4dad7580a5082
Ayas Nasih <ayasnasih@gmail.com>
2022-09-01 01:16:36 +0500

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,27 @@
+/**
+ * Ayas Nasih - S1600655
+ * Villa College - BSCHCS (Jan 2020)
+ */
+
 module.exports = {
-    "env": {
-        "browser": false,
-        "es2021": true
+    env: {
+        browser: false,
+        es2021: true,
     },
-    "extends": [
-        "airbnb-base",
-        "prettier"
+    extends: [
+        'prettier',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint",
-        "header"
+    plugins: [
+        '@typescript-eslint',
+        "header",
     ],
-    "rules": {
+    rules: {
         "header/header": [2, "config/header.ts"]
-    }
-}
+
+    },
+};
